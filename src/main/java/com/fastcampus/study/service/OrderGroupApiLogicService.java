fad74f8ddf84572b6b03af168c2bb422a3e6dc48
{JIHYE0705} <{jihyem75@gmail.com}>
2021-05-23 04:17:55 +0900

    OrderGroup API Create

---
+++ src/main/java/com/fastcampus/study/service/OrderGroupApiLogicService.java
@@ -0,0 +1,82 @@
+package com.fastcampus.study.service;
+
+
+import com.fastcampus.study.ifs.CrudInterface;
+import com.fastcampus.study.model.entity.OrderGroup;
+import com.fastcampus.study.model.network.Header;
+import com.fastcampus.study.model.network.request.OrderGroupApiRequest;
+import com.fastcampus.study.model.network.response.OrderGroupApiResponse;
+import com.fastcampus.study.repository.OrderGroupRepository;
+import com.fastcampus.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+
+        OrderGroup orderGroup = OrderGroup.builder()
+                .status(body.getStatus())
+                .orderType(body.getOrderType())
+                .revAddress(body.getRevAddress())
+                .revName(body.getRevName())
+                .paymentType(body.getPaymentType())
+                .totalPrice(body.getTotalPrice())
+                .totalQuantity(body.getTotalQuantity())
+                .orderAt(LocalDateTime.now())
+                .user(userRepository.getOne(body.getUserId()))
+                .build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
+
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+
+        OrderGroupApiResponse body= OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate())
+                .userId(orderGroup.getId())
+                .build();
+
+        return Header.OK(body);
+    }
+}
