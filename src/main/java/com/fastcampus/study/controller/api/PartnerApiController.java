e2649bbad01a5f3a58fb67305e7bdb2f31a142d3
{JIHYE0705} <{jihyem75@gmail.com}>
2021-05-30 20:27:02 +0900

    Abstraction code apply

---
+++ src/main/java/com/fastcampus/study/controller/api/PartnerApiController.java
@@ -0,0 +1,16 @@
+package com.fastcampus.study.controller.api;
+
+import com.fastcampus.study.controller.CrudController;
+import com.fastcampus.study.model.entity.Partner;
+import com.fastcampus.study.model.network.request.PartnerApiRequest;
+import com.fastcampus.study.model.network.response.PartnerApiResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/partner")
+public class PartnerApiController extends CrudController<PartnerApiRequest, PartnerApiResponse, Partner> {
+
+
+
+}
